1d1cad658edd68b600526ca76b4f8b821963da19
WinterInside <de_animator@mail.ru>
2021-12-03 16:31:55 +0300

    add accordion

--- source/js/script.js
+++ source/js/script.js
@@ -1,3 +1,51 @@
+
+var faq__accordion = (function (element) {
+	var _getItem = function (elements, className) {
+		var element = undefined;
+		elements.forEach(function (item) {
+		if (item.classList.contains(className)) {
+			element = item;
+		}
+	});
+	return element;
+	};
+	return function () {
+	var _mainElement = {},
+	_items = {},
+	_contents = {};
+	var _actionClick = function (e) {
+	if (!e.target.classList.contains('faq__accordion-item-header')) {
+		return;
+	}
+	e.preventDefault();
+	var header = e.target,
+	item = header.parentElement,
+	itemActive = _getItem(_items, 'show');
+	if (itemActive === undefined) {
+		item.classList.add('show');
+	} else {
+		itemActive.classList.remove('show');
+		if (itemActive !== item) {
+			item.classList.add('show');
+		}
+	}
+	},
+	_setupListeners = function () {
+		_mainElement.addEventListener('click', _actionClick);
+	};
+	return {
+		init: function (element) {
+			_mainElement = (typeof element === 'string' ? document.querySelector(element) : element);
+			_items = _mainElement.querySelectorAll('.faq__accordion-item');
+			_setupListeners();
+		}
+	}
+	}
+})();
+
+var faq__accordion1 = faq__accordion();
+faq__accordion1.init('#faq__accordion');
+
 window.addEventListener("DOMContentLoaded", function() {
   [].forEach.call( document.querySelectorAll('.form__input--tel'), function(input) {
   var keyCode;
